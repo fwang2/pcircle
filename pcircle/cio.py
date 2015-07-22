3b2fcbb05e201efdd2d626a660941cdd4bb85b6c
Feiyi Wang <fwang2@gmail.com>
2015-07-22 14:22:48 -0400

    error handling for readn() and writen()

--- pcircle/cio.py
+++ pcircle/cio.py
@@ -10,7 +10,10 @@ def readn(fd, size):
     tries = 0
     ret = ''
     while len(ret) < size:
-        buf = os.read(fd, size - len(ret))
+        try:
+            buf = os.read(fd, size - len(ret))
+        except OSError as e:
+            raise IOError(e.message)
         if len(buf) > 0:
             tries = MAX_TRIES
             ret += buf
@@ -20,8 +23,7 @@ def readn(fd, size):
         else:
             tries -= 1
             if tries < 0:
-                raise IOError
-
+                raise IOError("Failed after %s tries on read." % MAX_TRIES)
             time.sleep(SLEEP)
 
     return ret
@@ -31,13 +33,17 @@ def writen(fd, buf):
     n = 0
     tries = 0
     while n < size:
-        rc = os.write(fd, buf[n:])
+        try:
+            rc = os.write(fd, buf[n:])
+        except OSError as e:
+            raise IOError(e.message)
         if rc > 0:
             n += rc
             tries = MAX_TRIES
         else:
             tries -= 1
             if tries < 0:
-                raise IOError
+                raise IOError("Failed after %s tries on write" % MAX_TRIES)
             time.sleep(SLEEP)
+
     return n
